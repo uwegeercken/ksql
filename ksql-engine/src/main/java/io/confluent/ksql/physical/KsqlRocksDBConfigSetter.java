893ee254cb8881003430b42dd092e15e836e1512
Rohan Desai <desai.p.rohan@gmail.com>
2018-02-21 21:48:33 -0800

    Disable block cache

--- ksql-engine/src/main/java/io/confluent/ksql/physical/KsqlRocksDBConfigSetter.java
+++ ksql-engine/src/main/java/io/confluent/ksql/physical/KsqlRocksDBConfigSetter.java
@@ -1,6 +1,9 @@
 package io.confluent.ksql.physical;
 
 import org.apache.kafka.streams.state.RocksDBConfigSetter;
+import org.rocksdb.BlockBasedTableConfig;
+import org.rocksdb.BloomFilter;
+import org.rocksdb.Filter;
 import org.rocksdb.Options;
 
 import java.util.Map;
@@ -10,5 +13,11 @@ public class KsqlRocksDBConfigSetter implements RocksDBConfigSetter {
   public void setConfig(final String name, final Options options,
                         final Map<String, Object> config) {
     options.setAllowMmapReads(true);
+    final BlockBasedTableConfig tableConfig = new BlockBasedTableConfig();
+    tableConfig.setBlockCacheSize(0);
+    tableConfig.setBlockSize(4096);
+    //final Filter bloomFilter = new BloomFilter(10);
+    //tableConfig.setFilter(bloomFilter);
+    options.setTableFormatConfig(tableConfig);
   }
 }
